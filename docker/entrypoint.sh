a830e0dfdfaf5b6e7a4e2e4678bd0fd129f42114
minhuinie <nminhui@163.com>
2025-07-26 10:28:56 +0800

    fix docker deployment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+if [ -n "$TZ" ]; then
+  echo "Setting timezone to $TZ"
+  export TZ="$TZ"
+  echo "Current shell time: $(date)"
+fi
+
+if [ -n "$TSDB_URL" ]; then
+  echo "TSDB_URL is set to $TSDB_URL"
+  ESCAPED_URL=$(echo "$TSDB_URL" | sed 's/[\/&]/\\&/g')
+  sed -i "s|url: http://localhost:6041|url: $ESCAPED_URL|" /usr/local/taos/idmp/config/application.yml
+fi
+
 # necessary to make sure generator works
 cd /usr/local/taos/idmp
 
@@ -16,11 +28,6 @@ for i in {1..10}; do
   sleep 2
 done
 
-echo "Starting TDengine IDMP Chat Service..."
-export $(grep -v '^#' /usr/local/taos/idmp/service/tdengine-idmp-chat.env | xargs) || true
-nohup /usr/local/taos/idmp/venv/bin/python3 /usr/local/taos/idmp/chat/src/server.py 2>&1 &
-
-
 echo "Starting TDengine IDMP Service..."
 nohup /usr/bin/java -Xms1g -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/taos/ \
   -Dquarkus.config.locations=file:/usr/local/taos/idmp/config/application.yml \
@@ -34,5 +41,10 @@ for i in {1..20}; do
   fi
   sleep 2
 done
+
+echo "Starting TDengine IDMP Chat Service..."
+export $(grep -v '^#' /usr/local/taos/idmp/service/tdengine-idmp-chat.env | xargs) || true
+nohup /usr/local/taos/idmp/venv/bin/python3 /usr/local/taos/idmp/chat/src/server.py 2>&1 &
+
 
 tail -F /var/log/taos/*.log
