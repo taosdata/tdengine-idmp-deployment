609a047d8ce6b9a1b696c6362afe1636b1189fcb
danielclow <106956386+danielclow@users.noreply.github.com>
2026-01-27 22:44:09 +0800

    create workshop folder

---
+++ workshop/entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+if [ -n "$TZ" ]; then
+  echo "Setting timezone to $TZ"
+  export TZ="$TZ"
+  echo "Current shell time: $(date)"
+fi
+
+if [ -n "$TSDB_URL" ]; then
+  echo "TSDB_URL is set to $TSDB_URL"
+  ESCAPED_URL=$(echo "$TSDB_URL" | sed 's/[\/&]/\\&/g')
+  sed -i "s|url:[[:space:]]*http://localhost:6041|url: $ESCAPED_URL|" /usr/local/taos/idmp/config/application.yml
+fi
+
+# necessary to make sure generator works
+cd /usr/local/taos/idmp
+
+echo "Starting TDengine IDMP DB Service..."
+WEB_H2_EXTERNAL_NAMES=()
+[ -n "${H2_EXTERNAL_NAME}" ] && WEB_H2_EXTERNAL_NAMES=(-webExternalNames "${H2_EXTERNAL_NAME}")
+nohup /usr/bin/java -cp /usr/local/taos/idmp/lib/main/com.h2database.h2.jar org.h2.tools.Server \
+  -tcp -tcpAllowOthers -tcpPort 6039 -ifNotExists -web -webPort 6038 -webAllowOthers "${WEB_H2_EXTERNAL_NAMES[@]}" 2>&1 &
+
+for i in {1..10}; do
+  if netstat -tln | grep 6039; then
+    echo "tdengine-idmp-h2 started."
+    break
+  fi
+  sleep 2
+done
+
+echo "Starting TDengine IDMP Service..."
+nohup /usr/bin/java -Xms1g -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/taos/ \
+  -Dquarkus.config.locations=file:/usr/local/taos/idmp/config/application.yml \
+  -jar /usr/local/taos/idmp/quarkus-run.jar 2>&1 &
+
+echo "Waiting for TDengine IDMP Service to be ready..."
+for i in {1..20}; do
+  if netstat -tln | grep 6042; then
+    echo "TDengine IDMP is listening on port 6042"
+    break
+  fi
+  sleep 2
+done
+
+echo "Starting TDengine IDMP Chat Service..."
+export $(grep -v '^#' /usr/local/taos/idmp/service/tdengine-idmp-chat.env | xargs) || true
+nohup /usr/local/taos/idmp/venv/bin/python3 /usr/local/taos/idmp/chat/src/server.py 2>&1 &
+
+
+tail -F /var/log/taos/*.log
---
+++ workshop/init-anode.sql
@@ -0,0 +1,2 @@
+-- Initialize TDengine TSDB anode for TDGPT service
+create anode "tdengine-tdgpt:6035";
